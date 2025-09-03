db8b1ca21c7b34bf5edcd61afaa4050bd9496642
Jaime Fine <jaimefine6@gmail.com>
2025-09-03 23:20:55 +0800

    Finally making the repository 100% Rusty!!!

---
+++ bettergrep/target/debug/deps/bettergrep-c7d82f9696640008.d
@@ -0,0 +1,5 @@
+C:\Users\13647\Rust_Learning_Projects\bettergrep\target\debug\deps\bettergrep-c7d82f9696640008.exe: src\lib.rs
+
+C:\Users\13647\Rust_Learning_Projects\bettergrep\target\debug\deps\bettergrep-c7d82f9696640008.d: src\lib.rs
+
+src\lib.rs:
---
+++ bettergrep/target/debug/deps/bettergrep.d
@@ -0,0 +1,5 @@
+C:\Users\13647\Rust_Learning_Projects\bettergrep\target\debug\deps\bettergrep.exe: src/main.rs
+
+C:\Users\13647\Rust_Learning_Projects\bettergrep\target\debug\deps\bettergrep.d: src/main.rs
+
+src/main.rs:
