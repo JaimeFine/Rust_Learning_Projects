681305779b82b1279de3114b8f9bd0b1c5f42a0b
JaimeFine <jaimefine6@gmail.com>
2025-09-04 09:41:21 +0800

    Delete bettergrep/target directory

--- bettergrep/target/debug/deps/minigrep-b7264156205605e4.d
+++
@@ -1,7 +0,0 @@
-C:\Users\13647\Rust_Learning_Projects\minigrep\target\debug\deps\libminigrep-b7264156205605e4.rmeta: src\lib.rs
-
-C:\Users\13647\Rust_Learning_Projects\minigrep\target\debug\deps\libminigrep-b7264156205605e4.rlib: src\lib.rs
-
-C:\Users\13647\Rust_Learning_Projects\minigrep\target\debug\deps\minigrep-b7264156205605e4.d: src\lib.rs
-
-src\lib.rs:
