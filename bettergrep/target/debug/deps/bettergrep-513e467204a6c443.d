e5b37b1d7926f9cdbfb2f260e283a7a71b5a3fbd
Jaime Fan <jaimefine6@gmail.com>
2025-09-09 08:50:33 +0800

    Delete bettergrep/target/debug directory

--- bettergrep/target/debug/deps/bettergrep-513e467204a6c443.d
+++
@@ -1,5 +0,0 @@
-C:\Users\13647\Rust_Learning_Projects\bettergrep\target\debug\deps\libbettergrep-513e467204a6c443.rmeta: src/main.rs
-
-C:\Users\13647\Rust_Learning_Projects\bettergrep\target\debug\deps\bettergrep-513e467204a6c443.d: src/main.rs
-
-src/main.rs:
