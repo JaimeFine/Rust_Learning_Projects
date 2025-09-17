98e5d30195f1c43fb62ea4e8114bb01322fe45ec
JaimeFine <jaimefine6@gmail.com>
2025-09-17 16:21:27 +0800

    Created the web-server folder

---
+++ web-server/target/debug/deps/web_server.d
@@ -0,0 +1,5 @@
+C:\Users\13647\Rust_Learning_Projects\web-server\target\debug\deps\web_server.d: src\main.rs
+
+C:\Users\13647\Rust_Learning_Projects\web-server\target\debug\deps\web_server.exe: src\main.rs
+
+src\main.rs:
---
+++ web-server/target/debug/web-server.d
@@ -0,0 +1,1 @@
+C:\Users\13647\Rust_Learning_Projects\web-server\target\debug\web-server.exe: C:\Users\13647\Rust_Learning_Projects\web-server\src\main.rs
