ee40d730d64697d19541bae9e13b1a8a798c8eee
JaimeFine <jaimefine6@gmail.com>
2025-09-27 21:45:37 +0800

    Updated the number generator

--- miniprojects/random_number_generator/src/main.rs
+++ miniprojects/random_number_generator/src/main.rs
@@ -1,14 +1,18 @@
 // This is an exercise from the book Creative Projects for Rust Programmers C1
 
 use rand::prelude::*;
-use rand::thread_rng;
 
 fn main() {
-    let mut rng = thread_rng();
+    let mut rng = rand::rng();
 
     // Print 10 f32 numbers from 100 ~ 400:
     for _ in 0..10 {
-        let num: f32 = rng.gen_range(100.0..400.0);
+        let num: f32 = rng.random_range(100.0..400.0);
+        println!("{}", num);
+    }
+
+    for _ in 0..10 {
+        let num: i32 = rng.random_range(100..400);
         println!("{}", num);
     }
 }
