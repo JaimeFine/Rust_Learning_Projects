939fdbbf8206f268179ce5ea39e55003f11f9b0c
JaimeFine <jaimefine6@gmail.com>
2025-10-07 21:09:25 +0800

    Updated

--- miniprojects/print_words_in_colors/src/main.rs
+++ miniprojects/print_words_in_colors/src/main.rs
@@ -1,32 +1,54 @@
 use ansi_term::Colour;
 use std::io::{self, Write};
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     println!("Hello!");
-    println!("You can enter a something now, if you need help, enter help!");
+    println!("You can enter a something now, for exit, enter exit!");
+
+    let mut flag = false;
 
     loop {
         let mut phrase = String::new();
         let mut color = String::new();
 
-        println!("Enter the something:");
-        io::stdout.flush()?;
+        println!("Enter something:");
+        io::stdout().flush()?;
         io::stdin().read_line(&mut phrase)?;
         let phrase = phrase.trim();
 
         if phrase.is_empty() {
             println!("No valid color entered! Please try again:");
             continue;
+        } else if phrase == "exit" {
+            println!("Exiting...");
+            flag = true;
+            break;
+        } else {
+            break;
+        }
+    }
+
+    if !flag {
+        loop {
+            println!("Enter the color:");
+            io::stdout().flush()?;
+            io::stdin().read_line(&mut color);
+            let color = color.trim();
+
+            if color.is_empty() {
+                println!("No valid color entered! Please try again:");
+                continue;
+            }
         }
 
-        println!("Enter the color:");
-        io::stdout.flush()?;
-        io::stdin().read_line(&mut color);
-        let color = color.trim();
-
-        if color.is_empty() {
-            println!("No valid color entered! Please try again:");
-            continue;
+        let output = match color {
+            "red" => Colour::Red.paint(&phrase),
+            "green" => Colour::Green.paint(&phrase),
+            "yellow" => Colour::Yellow.paint(&phrase),
+            "blue" => Colour::Blue.paint(&phrase),
+            _ => println!(
+                "Sorry this color is not available"
+            )
         }
     }
 }
