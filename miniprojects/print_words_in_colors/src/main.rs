26013df41c26a58bd380c34eac13466a4d0e6a4c
JaimeFine <jaimefine6@gmail.com>
2025-10-07 22:34:17 +0800

    Fixed some bugs

--- miniprojects/print_words_in_colors/src/main.rs
+++ miniprojects/print_words_in_colors/src/main.rs
@@ -6,20 +6,20 @@ fn main() -> Result<(), std::io::Error> {
     println!("You can enter a something now, for exit, enter exit!");
 
     let mut flag = false;
+    let mut phrase = String::new();
+    let better_phrase = String::new();
+    let mut color = String::new();
 
     loop {
-        let mut phrase = String::new();
-        let mut color = String::new();
-
         println!("Enter something:");
         io::stdout().flush()?;
         io::stdin().read_line(&mut phrase)?;
-        let phrase = phrase.trim();
+        let better_phrase = phrase.trim().to_string();
 
-        if phrase.is_empty() {
+        if better_phrase.is_empty() {
             println!("No valid color entered! Please try again:");
             continue;
-        } else if phrase == "exit" {
+        } else if better_phrase == "exit" {
             println!("Exiting...");
             flag = true;
             break;
@@ -32,27 +32,29 @@ fn main() -> Result<(), std::io::Error> {
         loop {
             println!("Enter the color in lower-case:");
             io::stdout().flush()?;
-            io::stdin().read_line(&mut color);
-            let color = color.trim();
+            io::stdin().read_line(&mut color)?;
+            let color = color.trim().to_string();
 
             if color.is_empty() {
                 println!("No valid color entered! Please try again:");
                 continue;
+            } else {
+                break;
             }
         }
 
-        let output = match color {
-            "red" => Colour::Red.paint(&phrase),
-            "green" => Colour::Green.paint(&phrase),
-            "yellow" => Colour::Yellow.paint(&phrase),
-            "blue" => Colour::Blue.paint(&phrase),
-            "black" => Colour::Black.paint(&phrase),
-            "purple" => Colour::Purple.paint(&phrase),
-            "cyan" => Colour::Cyan.paint(&phrase),
-            "white" => Colour::White.paint(&phrase),
-            _ => println!(
+        let output = match color.as_str() {
+            "red" => Colour::Red.paint(&better_phrase),
+            "green" => Colour::Green.paint(&better_phrase),
+            "yellow" => Colour::Yellow.paint(&better_phrase),
+            "blue" => Colour::Blue.paint(&better_phrase),
+            "black" => Colour::Black.paint(&better_phrase),
+            "purple" => Colour::Purple.paint(&better_phrase),
+            "cyan" => Colour::Cyan.paint(&better_phrase),
+            "white" => Colour::White.paint(&better_phrase),
+            _ => Colour::White.paint(
                 "Sorry this color is not available"
-            )
+            ),
         };
 
         println!("Your output:");
